aeb7ddcdefd921bf326ad29e1fef7da78bfbe340
S Naveen Kumar <naveen39870@gmail.com>
2026-01-31 16:00:39 +0530

    Add files via upload

---
+++ scn.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class scn {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your salary");
+    System.out.println("enter your age");
+    System.out.println("enter your name:");
+    float salary=sc.nextFloat();
+    int age=sc.nextInt();
+    String name=sc.next();
+    String name1=sc.nextLine();
+    System.out.println(salary);
+    System.out.println(age);
+    System.out.println(name);
+    System.out.println(name1);
+
+   
+         
+        
+    }
+}
