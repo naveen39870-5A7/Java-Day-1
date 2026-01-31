5f6b47b3cbbc30e982daf634c48feeb71c1f5d1a
S Naveen Kumar <naveen39870@gmail.com>
2026-01-31 14:27:10 +0530

    Add files via upload

---
+++ Basicvarible.java
@@ -0,0 +1,21 @@
+public class Basicvarible {
+int a =10;
+int b =200;
+int c=a+b;
+
+  
+    public static void main(String[] args) {
+        // System.out.println("hello punith");
+        // System.out.println("Happy birthday puni");
+        // System.out.print("hello punith");
+        // System.out.println("hello punith");
+        // System.out.println("hello punith");
+        
+    int a =10;
+    int b =20;
+
+   
+         System.out.println(a+b);
+       
+    }}
+
