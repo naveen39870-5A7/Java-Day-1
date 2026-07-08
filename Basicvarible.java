40491818f4802fe67ab2fae30c403ddf918f026b
S Naveen Kumar <naveen39870@gmail.com>
2026-07-08 10:43:21 +0530

    Update Basicvarible.java

--- Basicvarible.java
+++ Basicvarible.java
@@ -1,10 +1,10 @@
 public class Basicvarible {  
     public static void main(String[] args) {
-        System.out.println("hello punith");
-        System.out.println("Happy birthday puni");
-        System.out.print("hello punith");
-        System.out.println("hello punith");
-        System.out.println("hello punith");
+        // System.out.println("hello punith");
+        // System.out.println("Happy birthday puni");
+        // System.out.print("hello punith");
+        // System.out.println("hello punith");
+        // System.out.println("hello punith");
         
     int a =10;
     int b =20;
